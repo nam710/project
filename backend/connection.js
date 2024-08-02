0d9db7c996903a6152c2ad5a8910a41d0ef878b1
nam710 <namanchawla029@gmail.com>
2024-08-02 21:53:44 +0530

    fixed server endpoints

--- backend/connection.js
+++ backend/connection.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
-const dburl = require('./config').db_url;
-mongoose.connect(dburl)
-.then((data) => {
-    console.log('Database successfully connected');
-})
-.catch((data) => {
+const mongoose = require("mongoose");
+const dburl = require("./config").db_url;
+mongoose
+  .connect(dburl)
+  .then((data) => {
+    console.log("Database successfully connected");
+  })
+  .catch((data) => {
     console.error(err);
-})
+  });
 module.exports = mongoose;
